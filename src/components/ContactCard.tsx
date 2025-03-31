b85b2f5304255361676be2f628287cdd2302bf23
shaylonmizukami <smizukami15@gmail.com>
2025-03-30 15:50:57 -1000

    list contacts page mockup finished in 25 minutes

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Card, Image } from 'react-bootstrap';
+// eslint-disable-next-line import/extensions
+import { Contact } from '@/lib/validationSchemas';
+
+/* Renders a single Contact. See list/page.tsx. */
+const ContactCard = ({ contact }: { contact: Contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
